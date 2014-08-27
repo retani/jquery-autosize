72dc6f5cbb61bd96c683446bc6137d0ac165bb24
Kelly Copley <copleykj@gmail.com>
2014-08-27 12:04:53 -0400

    update package.js for 0.9.0

--- package.js
+++ package.js
@@ -1,11 +1,12 @@
 Package.describe({
-  summary: 'Automatically adjust textarea height based on user input.'
+  summary: 'Automatically adjust textarea height based on user input.',
+  version: '1.17.8',
+  git: "https://github.com/copleykj/jquery-autosize.git"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR@0.9.0");
   api.use('jquery', 'client');
-  api.add_files([
-    'lib/jquery.autosize.js'
-  ], 'client'
-  );
+  api.add_files(['lib/jquery.autosize.js'], 'client');
+  
 });
